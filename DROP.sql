76e76b7e8ceae475aed739a991e89e1324a3e965
RafaelM28 <rafaelmarcano2804@gmail.com>
2024-06-21 02:39:35 -0400

    DROP de Tablas

---
+++ DROP.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION borrar_tablas() RETURNS void AS $$
+DECLARE
+  cur_name text;
+BEGIN
+  FOR cur_name IN (SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE') LOOP
+    EXECUTE 'DROP TABLE IF EXISTS public.' || quote_ident(cur_name) || ' CASCADE';
+  END LOOP;
+END;
+$$ LANGUAGE plpgsql VOLATILE;
+
+SELECT borrar_tablas();
