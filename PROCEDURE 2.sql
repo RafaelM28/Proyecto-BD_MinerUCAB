293747957eea07830306a3903dbc7bec2dffc3a1
RafaelM28 <rafaelmarcano2804@gmail.com>
2024-06-25 13:09:22 -0400

    Vista de listas en Flask
    Actualización html, funciones sql y app.py para mostrar vistas de listas de tablas

--- PROCEDURE 2.sql
+++ PROCEDURE 2.sql
@@ -6,9 +6,7 @@ AS $$
 BEGIN
     -- La consulta selecciona los campos de la tabla cliente
     RETURN QUERY SELECT C.persona_jur_codigo, C.persona_jur_rif, C.persona_jur_denominacion_comercial, C.persona_jur_direccion_fiscal
-    FROM cliente C
-    -- Limita el número de resultados a 30
-    LIMIT 30;
+    FROM cliente C;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -19,9 +17,7 @@ AS $$
 BEGIN
     -- La consulta selecciona los campos de la tabla aliado
     RETURN QUERY SELECT A.persona_jur_codigo, A.persona_jur_rif, A.persona_jur_denominacion_comercial, A.persona_jur_direccion_fiscal
-    FROM aliado A
-    -- Limita el número de resultados a 30
-    LIMIT 30;
+    FROM aliado A;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -33,9 +29,7 @@ BEGIN
     -- La consulta selecciona los campos de la tabla empleado
     RETURN QUERY SELECT E.empleado_codigo, E.empleado_cedula, E.empleado_primer_nombre||' '|| E.empleado_primer_apellido AS empleado_nombre, A.persona_jur_denominacion_comercial, C.cargo_nombre
     FROM empleado E LEFT JOIN  aliado A on E.fk_aliado_2 = A.persona_jur_codigo, contrato_empleado CE, cargo C
-    WHERE E.empleado_codigo = CE.fk_empleado AND CE.fk_cargo = C.cargo_codigo
-    -- Limita el número de resultados a 30
-    LIMIT 30;
+    WHERE E.empleado_codigo = CE.fk_empleado AND CE.fk_cargo = C.cargo_codigo;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -47,14 +41,16 @@ BEGIN
     -- La consulta selecciona los campos de la tabla usuario
  RETURN QUERY SELECT U.usuario_codigo,
     CASE
+        -- Si el campo fk_aliado no es nulo, se muestra el nombre del aliado
         WHEN U.fk_aliado IS NOT NULL THEN CAST((SELECT A.persona_jur_razon_social FROM aliado A WHERE A.persona_jur_codigo = U.fk_aliado) AS text)
+        -- Si el campo fk_cliente no es nulo, se muestra el nombre del cliente
         WHEN U.fk_cliente IS NOT NULL THEN CAST((SELECT C.persona_jur_razon_social FROM cliente C WHERE C.persona_jur_codigo = U.fk_cliente) AS text)
+        -- Si el campo fk_empleado no es nulo, se muestra el nombre del empleado
         WHEN U.fk_empleado IS NOT NULL THEN (SELECT E.empleado_primer_nombre||' '|| E.empleado_primer_apellido AS empleado_nombre FROM empleado E WHERE E.empleado_codigo = U.fk_empleado)
     END,
                   U.usuario_nombre, R.rol_nombre
 FROM usuario U, rol R
-WHERE U.fk_rol = R.rol_codigo
-LIMIT 30;
+WHERE U.fk_rol = R.rol_codigo;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -65,9 +61,7 @@ AS $$
 BEGIN
     -- La consulta selecciona los campos de la tabla privilegio
     RETURN QUERY SELECT P.privilegio_codigo, P.privilegio_nombre
-    FROM privilegio P
-    -- Limita el número de resultados a 30
-    LIMIT 30;
+    FROM privilegio P;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -78,8 +72,119 @@ AS $$
 BEGIN
     -- La consulta selecciona los campos de la tabla rol
     RETURN QUERY SELECT R.rol_codigo, R.rol_nombre
-    FROM rol R
-    -- Limita el número de resultados a 30
-    LIMIT 30;
+    FROM rol R;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Creación de una función lista_minerales()
+CREATE OR REPLACE FUNCTION lista_minerales()
+RETURNS TABLE (mineral_codigo SMALLINT, mineral_nombre VARCHAR(50), mineral_tipo VARCHAR(15), inventario_cantidad INTEGER)
+AS $$
+BEGIN
+    -- La consulta selecciona los campos de la tabla mineral
+    RETURN QUERY SELECT M.mineral_codigo, M.mineral_nombre, M.mineral_tipo, I.inventario_producto_cantidad_total
+    FROM mineral M, inventario_producto I
+    WHERE I.inventario_producto_codigo IN (    SELECT MAX(inventario_producto_codigo)
+                                                                        FROM Inventario_Producto IP
+                                                                         WHERE IP.fk_mineral = M.mineral_codigo)
+    ORDER BY M.mineral_codigo;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Creación de una función lista_recursos()
+CREATE OR REPLACE FUNCTION lista_recursos()
+RETURNS TABLE (recurso_codigo SMALLINT, recurso_nombre VARCHAR(50), recurso_tipo VARCHAR(30), modelo_nombre VARCHAR(50), inventario_cantidad INTEGER)
+AS $$
+BEGIN
+    -- La consulta selecciona los campos de la tabla recurso
+    RETURN QUERY SELECT R.recurso_codigo, R.recurso_nombre, TR.tipo_recurso_nombre, M.modelo_nombre, I.inventario_recurso_cantidad_total
+    FROM tipo_recurso TR, modelo M, recurso R LEFT JOIN inventario_recurso I ON R.recurso_codigo = I.fk_recurso
+    WHERE R.fk_tipo_recurso = TR.tipo_recurso_codigo AND R.fk_modelo_1 = M.modelo_codigo AND R.fk_modelo_2 = M.fk_marca
+    ORDER BY R.recurso_codigo;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Creación de una función obtener_tipo_operacion_inventario()
+-- Esta función devuelve el tipo de operación de un inventario perteneciente a una compra o proyecto
+CREATE OR REPLACE FUNCTION obtener_tipo_operacion_inventario(inventario_producto_codigo SMALLINT)
+RETURNS text
+LANGUAGE plpgsql
+AS $$
+        DECLARE
+        tipo_operacion text;
+BEGIN
+    -- Verifica si el código existe en la tabla Detalle_Pedido_Compra
+    IF EXISTS (SELECT 1 FROM Detalle_Pedido_Compra AS DPC WHERE DPC.fk_inventario_producto_1 = inventario_producto_codigo) THEN
+        tipo_operacion = 'Compra';
+    -- Si no, verifica si el código existe en la tabla Proyecto_Ejecucion
+    ELSIF EXISTS (SELECT 1 FROM Proyecto_Ejecucion AS PE WHERE PE.fk_inventario_producto_1 = inventario_producto_codigo) THEN
+        tipo_operacion = 'Proyecto';
+    END IF;
+    RETURN tipo_operacion;
+END;
+$$;
+
+-- Creación de una función lista_inventario()
+CREATE OR REPLACE FUNCTION lista_inventario()
+RETURNS TABLE (inventario_codigo SMALLINT, mineral_nombre VARCHAR(30), inventario_product INTEGER, inventario_total INTEGER, inventario_tipo VARCHAR(15), inventario_fecha DATE, tipo_operacion text)
+AS $$
+BEGIN
+    -- La consulta selecciona los campos de la tabla inventario
+    RETURN QUERY SELECT I.inventario_producto_codigo, M.mineral_nombre, I.inventario_producto_operacion, I.inventario_producto_cantidad_total,
+                                          I.inventario_producto_tipo_operacion, I.inventario_producto_fecha_movimiento,
+        CASE
+            -- Si el tipo de operación es 'Egreso', se muestra 'Venta'
+        WHEN I.inventario_producto_tipo_operacion = 'Egreso' THEN (SELECT 'Venta')
+            -- Si el tipo de operación es 'Ingreso', se muestra el resultado de la función obtener_tipo_operacion_inventario()
+        WHEN I.inventario_producto_tipo_operacion = 'Ingreso' THEN (SELECT obtener_tipo_operacion_inventario(I.inventario_producto_codigo))
+        END
+    FROM inventario_producto I, mineral M
+    WHERE I.fk_mineral = M.mineral_codigo;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Creación de una función lista_solicitudes()
+CREATE OR REPLACE FUNCTION lista_solicitudes()
+RETURNS TABLE (pedido_codigo SMALLINT, pedido_fecha_emision DATE, aliado VARCHAR(30), pedido_estatus VARCHAR(30))
+AS $$
+BEGIN
+    -- La consulta selecciona los campos de la tabla solicitud
+    RETURN QUERY SELECT PC.pedido_compra_numero, PC.pedido_compra_fecha_emision, A.persona_jur_denominacion_comercial, EP.estatus_pedido_nombre
+    FROM pedido_compra PC, aliado A, historico_estatus_pedido_compra HPC, estatus_pedido EP
+    WHERE PC.fk_aliado = A.persona_jur_codigo AND PC.pedido_compra_numero = HPC.fk_pedido_compra_1 AND HPC.hist_est_pedido_compra_codigo = EP.estatus_pedido_codigo;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Creación de una función lista_proyectos_config()
+CREATE OR REPLACE FUNCTION lista_proyectos_config()
+RETURNS TABLE (proyecto_codigo SMALLINT, proyecto_nombre VARCHAR(30), mineral_nombre VARCHAR(30))
+AS $$
+BEGIN
+    -- La consulta selecciona los campos de la tabla proyecto
+    RETURN QUERY SELECT PC.proyecto_config_codigo, PC.proyecto_config_nombre, M.mineral_nombre
+    FROM proyecto_configuracion PC, mineral M
+    WHERE PC.fk_mineral = M.mineral_codigo;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Creación de una función lista_etapas_config()
+CREATE OR REPLACE FUNCTION lista_etapas_config()
+RETURNS TABLE (etapa_codigo SMALLINT, etapa_nombre VARCHAR(30), etapa_numero SMALLINT)
+AS $$
+BEGIN
+    -- La consulta selecciona los campos de la tabla etapa
+    RETURN QUERY SELECT EC.etapa_config_codigo, EC.etapa_config_nombre, EC.etapa_config_numero
+    FROM etapa_configuracion EC;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Creación de una función lista_actividades_config()
+CREATE OR REPLACE FUNCTION lista_actividades_config()
+RETURNS TABLE (actividad_codigo SMALLINT, actividad_nombre VARCHAR(45))
+AS $$
+BEGIN
+    -- La consulta selecciona los campos de la tabla actividad
+    RETURN QUERY SELECT AC.actividad_config_codigo, AC.actividad_config_nombre
+    FROM actividad_configuracion AC;
 END;
 $$ LANGUAGE plpgsql;
--- app.py
+++ app.py
@@ -127,5 +127,124 @@ def lista_roles():
     # Renderización de la plantilla HTML para 'lista_roles', pasando los datos de roles al template
     return render_template('Lista_Roles/lista_roles.html', roles=roles)
 
+# Definición de la ruta '/lista_minerales'
+@app.route('/lista_minerales')
+def lista_minerales():
+    # Establecimiento de la conexión y creación de un cursor para ejecutar consultas
+    cur = connection().cursor()
+    
+    # Ejecución de la función almacenada 'lista_minerales' que retorna una lista de minerales
+    cur.execute("SELECT * FROM lista_minerales()")
+    minerales = cur.fetchall()  
+    
+    # Cierre del cursor y de la conexión a la base de datos
+    cur.close()  
+    connection().close()  
+    
+    # Renderización de la plantilla HTML para 'lista_minerales', pasando los datos de minerales al template
+    return render_template('Lista_Minerales/lista_minerales.html', minerales=minerales)
+
+# Definición de la ruta '/lista_recursos'
+@app.route('/lista_recursos')
+def lista_recursos():
+    # Establecimiento de la conexión y creación de un cursor para ejecutar consultas
+    cur = connection().cursor()
+    
+    # Ejecución de la función almacenada 'lista_recursos' que retorna una lista de recursos
+    cur.execute("SELECT * FROM lista_recursos()")
+    recursos = cur.fetchall()  
+    
+    # Cierre del cursor y de la conexión a la base de datos
+    cur.close()  
+    connection().close()  
+    
+    # Renderización de la plantilla HTML para 'lista_recursos', pasando los datos de recursos al template
+    return render_template('Lista_Recursos/lista_recursos.html', recursos=recursos)
+
+# Definición de la ruta '/lista_inventario'
+@app.route('/lista_inventario')
+def lista_inventario():
+    # Establecimiento de la conexión y creación de un cursor para ejecutar consultas
+    cur = connection().cursor()
+    
+    # Ejecución de la función almacenada 'lista_inventario' que retorna una lista de inventario
+    cur.execute("SELECT * FROM lista_inventario()")
+    inventario = cur.fetchall()  
+    
+    # Cierre del cursor y de la conexión a la base de datos
+    cur.close()  
+    connection().close()  
+    
+    # Renderización de la plantilla HTML para 'lista_inventario', pasando los datos de inventario al template
+    return render_template('Lista_Inventario_Operaciones/lista_operaciones.html', inventario=inventario)
+
+# Definición de la ruta '/lista_solicitudes'
+@app.route('/lista_solicitudes')
+def lista_solicitudes():
+    # Establecimiento de la conexión y creación de un cursor para ejecutar consultas
+    cur = connection().cursor()
+    
+    # Ejecución de la función almacenada 'lista_solicitudes' que retorna una lista de solicitudes
+    cur.execute("SELECT * FROM lista_solicitudes()")
+    solicitudes = cur.fetchall()  
+    
+    # Cierre del cursor y de la conexión a la base de datos
+    cur.close()  
+    connection().close()  
+    
+    # Renderización de la plantilla HTML para 'lista_solicitudes', pasando los datos de solicitudes al template
+    return render_template('Lista_Solicitudes/lista_solicitudes.html', solicitudes=solicitudes)
+
+# Definición de la ruta '/lista_proyectos_config'
+@app.route('/lista_proyectos_config')
+def lista_proyectos():
+    # Establecimiento de la conexión y creación de un cursor para ejecutar consultas
+    cur = connection().cursor()
+    
+    # Ejecución de la función almacenada 'lista_proyectos' que retorna una lista de proyectos
+    cur.execute("SELECT * FROM lista_proyectos_config()")
+    proyectos = cur.fetchall()  
+    
+    # Cierre del cursor y de la conexión a la base de datos
+    cur.close()  
+    connection().close()  
+    
+    # Renderización de la plantilla HTML para 'lista_proyectos', pasando los datos de proyectos al template
+    return render_template('Lista_Proyectos/lista_proyectos.html', proyectos=proyectos)
+
+# Definición de la ruta '/lista_etapas_config'
+@app.route('/lista_etapas_config')
+def lista_etapas():
+    # Establecimiento de la conexión y creación de un cursor para ejecutar consultas
+    cur = connection().cursor()
+    
+    # Ejecución de la función almacenada 'lista_etapas' que retorna una lista de etapas
+    cur.execute("SELECT * FROM lista_etapas_config()")
+    etapas = cur.fetchall()  
+    
+    # Cierre del cursor y de la conexión a la base de datos
+    cur.close()  
+    connection().close()  
+    
+    # Renderización de la plantilla HTML para 'lista_etapas', pasando los datos de etapas al template
+    return render_template('Lista_Etapas/lista_etapas.html', etapas=etapas)
+
+# Definición de la ruta '/lista_actividades_config'
+@app.route('/lista_actividades_config')
+def lista_actividades():
+    # Establecimiento de la conexión y creación de un cursor para ejecutar consultas
+    cur = connection().cursor()
+    
+    # Ejecución de la función almacenada 'lista_actividades' que retorna una lista de actividades
+    cur.execute("SELECT * FROM lista_actividades_config()")
+    actividades = cur.fetchall()  
+    
+    # Cierre del cursor y de la conexión a la base de datos
+    cur.close()  
+    connection().close()  
+    
+    # Renderización de la plantilla HTML para 'lista_actividades', pasando los datos de actividades al template
+    return render_template('Lista_Actividades/lista_actividades.html', actividades=actividades)
+
 if __name__ == '__main__': 
     app.run(debug=True) 
